e845718e48f3f70c99f64830c9e08d7bf6ba2179
Muhesh Kumar <muhesh4102002@gmail.com>
2022-10-19 23:49:02 +0530

    fix: fix deploy issues

--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react'
 
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps, session }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps<{ session: Session; }>) => {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   )
