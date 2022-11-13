28eafc343073e7d18752944e81c6fef97b4cb67a
Muhesh Kumar <muhesh4102002@gmail.com>
2022-11-13 23:38:28 +0530

    feat: add styles to generated schedule

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextPage } from 'next';
 
 import GeneralPageLayout from '@layouts/GeneralPageLayout';
@@ -7,31 +8,53 @@ import { getCourseStartWeeks } from '@utils/findElectives';
 
 const Schedule: NextPage = () => {
   const chosenElective = useElectiveStore((state) => state.chosenElective);
-  console.log(chosenElective);
   const courseStartWeeks = getCourseStartWeeks(chosenElective);
-  console.log(courseStartWeeks);
+  const weeks = Object.entries(courseStartWeeks).map(
+    ([weekNumber, _]) => weekNumber,
+  );
+  const coursesPerWeek = Object.entries(courseStartWeeks).map(
+    ([_, courses]) => courses,
+  );
 
   return (
     <GeneralPageLayout>
-      <div className="flex flex-col gap-5 items-center">
-        {Object.entries(courseStartWeeks).map(
-          ([weekNumber, scheduledCourses]) => {
-            return (
-              <div key={weekNumber} className="flex gap-5">
-                <p className="text-xl font-semibold">Week {weekNumber}</p>
-                <div className="flex gap-5">
-                  {scheduledCourses.map((scheduledCourse) => {
+      <div className="flex flex-col gap-10 h-full justify-center items-center ">
+        <h2 className="text-2xl">
+          Generated Schedule for{' '}
+          <span className="font-bold">{chosenElective}</span>
+        </h2>
+        <div className="flex gap-5">
+          <div className="flex flex-col gap-5">
+            {weeks.map((weekNumber) => {
+              return (
+                <div key={weekNumber} className="flex flex-col border-2">
+                  <p className="text-2xl font-semibold p-2">
+                    Week {weekNumber}:{' '}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="flex flex-col gap-5">
+            {coursesPerWeek.map((courses) => {
+              return (
+                <div key={courses[0]} className="flex gap-5">
+                  {courses.map((course) => {
                     return (
-                      <p className="text-xl " key={scheduledCourse}>
-                        {scheduledCourse}
+                      <p
+                        className="text-2xl bg-primaryDark text-white border-2 border-primaryDark rounded-md px-4 py-2"
+                        key={course}
+                      >
+                        {course}
                       </p>
                     );
                   })}
                 </div>
-              </div>
-            );
-          },
-        )}
+              );
+            })}
+          </div>
+        </div>
       </div>
     </GeneralPageLayout>
   );
